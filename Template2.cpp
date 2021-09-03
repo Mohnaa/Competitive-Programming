622e0e49eb03f68439f364b5f7f4671f74323087
Mohnaa Ghosh <71881679+Mohnaa@users.noreply.github.com>
2021-09-03 15:02:46 +0530

    Create Template2.cpp

---
+++ Template2.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#define vi vector<int>
+#define pii pair<int,int>
+#define vpi vector<pii>
+#define rep(i,a,b) for(int i=a;i<i<b;i++)
+#define ff first
+#define ss second
+typedef unsigned long long int ulli;
+typedef long long int lli;
+typedef long int li;
+typedef pair<int, int> pii;
+typedef map<int, int> mii;
+typedef map<string, int> msi;
+typedef map<string, string> mss;
+typedef vector<int> vi;
+typedef vector<lli> vlli;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+#define MOD1 1000000007
+#define MOD2 1000000009
+
+using namespace std;
+
+signed main(){
+    return 0;
+}
